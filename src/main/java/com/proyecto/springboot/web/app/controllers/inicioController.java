483791c380729e7df8fd9fc712a9469c00cd54a3
Fco-Reyesf <freyes.info@gmail.com>
2022-03-21 23:24:30 -0300

    rutas base

--- src/main/java/com/proyecto/springboot/web/app/controllers/inicioController.java
+++ src/main/java/com/proyecto/springboot/web/app/controllers/inicioController.java
@@ -3,13 +3,15 @@ package com.proyecto.springboot.web.app.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/home")		// ruta de primer nivel, base para todos los elementos del controlador
 public class inicioController {
 	// llamada a las vistas
 	
 	// vista principal
-	@GetMapping({"/","/index","/home"})
+	@GetMapping({"/index","/saludo"})
 	public String inicio(Model modelo) {
 		modelo.addAttribute("nombreParametro", "valor del parametro");
 		return "index";
