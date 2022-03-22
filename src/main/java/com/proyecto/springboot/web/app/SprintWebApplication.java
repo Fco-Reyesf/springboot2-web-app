6ea5bbb72c039c61e6c32386880ba0c553492968
Fco-Reyesf <freyes.info@gmail.com>
2022-03-21 22:26:02 -0300

    primer push

---
+++ src/main/java/com/proyecto/springboot/web/app/SprintWebApplication.java
@@ -0,0 +1,14 @@
+package com.proyecto.springboot.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintWebApplication {
+
+	// arranque del programa
+	public static void main(String[] args) {
+		SpringApplication.run(SprintWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/springboot/web/app/SprintWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.springboot.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
