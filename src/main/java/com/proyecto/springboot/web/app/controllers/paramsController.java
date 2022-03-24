ae2ca4c1545b6892c95eab702b9d087c95060b12
Fco-Reyesf <freyes.info@gmail.com>
2022-03-23 22:42:47 -0300

    envio varios params

--- src/main/java/com/proyecto/springboot/web/app/controllers/paramsController.java
+++ src/main/java/com/proyecto/springboot/web/app/controllers/paramsController.java
@@ -1,5 +1,7 @@
 package com.proyecto.springboot.web.app.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +21,23 @@ public class paramsController {
 	@GetMapping("/string")
 	public String param(@RequestParam(required = false) String texto , Model modelo){
 		modelo.addAttribute("resultado", "El parametro es: " + texto);
+		return "params/ver";
+	}
+	
+	@GetMapping("/varios-params")
+	public String param(@RequestParam(required = false) String paramUno, @RequestParam(required = false) Integer paramDos , Model modelo){
+		modelo.addAttribute("resultado", "El parametro uno es: " + paramUno + " el parametro es: " + paramDos);
+		return "params/ver";
+	}
+	
+	
+	@GetMapping("/varios-params-request")
+	public String param(HttpServletRequest request , Model modelo){
+		try {
+			String paramUno = request.getParameter("paramUno");
+			Integer paramDos = Integer.parseInt(request.getParameter("paramDos"));
+		modelo.addAttribute("resultado", "El parametro uno es: " + paramUno + " el parametro es: " + paramDos);
+		} catch (NumberFormatException e) {}
 		return "params/ver";
 	}
 }
